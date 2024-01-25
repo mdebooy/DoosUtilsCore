d84d735d41257013fa4fda33aa556c31c2a3da6e
Marco DE BOOIJ <marco.developer@debooy.eu>
2024-01-25 15:37:35 +0100

    static String voor dd/MM/yyyy.

--- src/main/java/eu/debooy/doosutils/DoosConstants.java
+++ src/main/java/eu/debooy/doosutils/DoosConstants.java
@@ -33,6 +33,7 @@ public final class DoosConstants {
   public static final String  WAAR    = "J";
 
   public static final String  DATUM         = "dd-MM-yyyy";
+  public static final String  DATUM_SLASH   = "dd/MM/yyyy";
   public static final String  DATUM_TIJD    = "dd-MM-yyyy HH:mm:ss";
   public static final String  SORTEERDATUM  = "yyyyDDD";
 
